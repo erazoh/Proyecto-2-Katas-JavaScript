e30ba74a010a2f9c2db212cc4c6f8e602732437d
Sebastian Erazo <eserazoh@gmail.com>
2025-12-09 08:54:31 +0100

    ejejrcicioDOM completo

--- EjerciciosDOM/Ejercicio 4/script.js
+++ EjerciciosDOM/Ejercicio 4/script.js
@@ -1,6 +1,41 @@
 // 1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el
 // evento click que ejecute un console log con la información del evento del click
 
+    const clickEvent = document.querySelector(".click");
+    clickEvent.type="button";
+    clickEvent.value = "ejercicio 1.1"
+    clickEvent.addEventListener("click",(event)=> { 
+        console.log(event); //evento
+        console.log(event.target); //boton al que se le hace target
+    })
+    
+    
+
+
 // 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
 
+    const focusEvent = document.querySelector(".focus");
+    focusEvent.value = "Seleccióname";
+    focusEvent.addEventListener("focus", (event) =>{
+        event.target.style.background = "black";
+        event.target.style.color = "white";
+        event.target.value = "Estas haciendo foco aquí";
+        console.log(event.target)
+        console.log(event)
+    }  )
+
+    focusEvent.addEventListener("blur",(event) =>{
+        event.target.style.background = "";
+        event.target.style.color = "";
+        event.target.value = "Seleccióname :(";
+        console.log(event.target)
+        console.log(event)
+    }  )
+
+
 // 1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
+
+    const inputEvent = document.querySelector(".value");
+     inputEvent.addEventListener("input",(event) =>{
+        console.log("valor :",event.target.value)
+    }  )
--- EjerciciosDOM/Ejercicio 5/script.js
+++ EjerciciosDOM/Ejercicio 5/script.js
@@ -8,3 +8,37 @@ const albums = [
   "Painkiller",
   "Iron Fist",
 ];
+
+//
+body = document.body;
+
+// haremos un titulo
+let title = document.createElement("h2");
+title.textContent = "Álbumes";
+body.appendChild(title);
+
+//crearemos una lista
+let lista = document.createElement("ul");
+
+//recorremos la lista albums y la gregamos directamente
+let contador = 0;
+albums.forEach((element) => {
+  //creamos el elemento de la lista
+  li = document.createElement("li");
+  //creamos un elemto parrafo
+  p = document.createElement("p");
+  // le agregamos lo que recorre la lista
+  p.textContent = element;
+  //lo agregamos a lista
+  li.appendChild(p)
+  //agreamos el elemento imagen con un src que se va auto incrimentando
+  img = document.createElement("img");
+  img.src =`https://picsum.photos/300/200?random=${contador}`
+  contador++;
+  //agregamos la imagen al elemto lista y en css le damos formato
+  li.appendChild(img);
+  lista.appendChild(li);
+});
+
+//agregamos la lista al body
+body.appendChild(lista);
